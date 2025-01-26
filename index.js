bbf391b0c6f99d68788d4f00b0f73d2f4d1e48d9
Marko2155 <98662057+Marko2155@users.noreply.github.com>
2025-01-26 10:24:10 +0100

    Fixed a bug... in a project about finding bugs.. lolol

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("clickme").addEventListener(function() {
+document.getElementById("clickme").addEventListener("click", function() {
     let arr = new Uint8Array(4);  // An array with 4 bytes (0-3 index)
     arr[0] = 10;
     arr[4] = 20;  // Out of bounds (array length is 4, valid indices are 0-3)
